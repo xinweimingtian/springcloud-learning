e93328e9773f080d2d54eb5134945f53bb116374
xinweimingtian <306374349@qq.com>
2020-07-06 17:40:15 +0800

    Hystrix Dashboard: 断路器执行监控-页面bug说明

--- hystrix-dashboard/src/main/java/com/ming/cloud/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/ming/cloud/HystrixDashboardApplication.java
@@ -11,6 +11,11 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 public class HystrixDashboardApplication {
 
+    // 说明  如果jquery版本太高  那么页面将无法展示 会出现e.indexOf is not a function 错误
+    // 需要修改spring-cloud-netflix-hystrix-dashboard-2.2.3.RELEASE
+    // templates\hystrix\monitor.ftlh
+    // 把 $(window).load(function() {
+    // 改为 $(window).on('load', function(){
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardApplication.class, args);
     }
