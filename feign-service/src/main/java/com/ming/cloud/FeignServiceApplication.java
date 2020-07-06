8e52c5fa9fffcbf3b50321b934c46b200d18aace
xinweimingtian <306374349@qq.com>
2020-07-06 17:44:41 +0800

    Feign初始化

---
+++ feign-service/src/main/java/com/ming/cloud/FeignServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ming.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignServiceApplication.class, args);
+    }
+
+}
