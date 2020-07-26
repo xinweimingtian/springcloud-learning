bc3afac8eaa176d7d140bdc5cfab4dc04d592486
xinweimingtian <306374349@qq.com>
2020-07-26 23:30:45 +0800

    config client 配置中心客户端

---
+++ config-client/src/main/java/com/ming/cloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.ming.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description ConfigClientController
+ * @Author Administrator
+ * @Date 2020/7/26 23:11 2020
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+}
